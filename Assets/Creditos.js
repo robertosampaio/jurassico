6488939cbaded394c76ba9c9b302fed346097f04
robertosampaio <robertosampaio@id.uff.br>
2013-11-30 16:05:48 -0200

    Menu e creditos iniciados

---
+++ Assets/Creditos.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var iconYusim : Texture2D;
+var iconRoberto : Texture2D;
+var iconMatheus : Texture2D;
+var iconFelipe : Texture2D;
+
+function Start () {
+
+}
+
+function OnGUI () {
+	GUI.Box (Rect (0, 0, 400, 200), GUIContent("Yusim", iconYusim));
+	GUI.Box (Rect (Screen.width - 400,0, 400, 200), GUIContent("Matheus", iconMatheus));
+	GUI.Box (Rect (0,Screen.height - 200, 400, 200), GUIContent("Roberto", iconRoberto));
+	GUI.Box (Rect (Screen.width - 400,Screen.height - 200, 400, 200), GUIContent("Filipe", iconFelipe));
+	
+	if (GUI.Button (Rect (780,400,400,40), "Voltar")) {
+		Application.LoadLevel (0);
+	}
+}
+
+function Update () {
+
+}
---
+++ Assets/Scripts/Menu.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var fontStyle: FontStyle;
+
+function Start(){
+    
+}
+ 
+function OnGUI () {
+	GUI.Box (Rect (780,350,400,200), "Running Time");
+
+	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+	if (GUI.Button (Rect (780,400,400,40), "Iniciar gueeeime")) {
+		Application.LoadLevel (1);
+	}
+
+	// Make the second button.
+	if (GUI.Button (Rect (780,450,400,40), "Creditos")) {
+		Application.LoadLevel (2);
+	}
+}
+function Update () {
+
+}
